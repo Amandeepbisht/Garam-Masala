cbaeedb0e6ee906a6c28b13dd7c092dc0f1581d3
Amandeepbisht <amandeepbisht1994@gmail.com>
2021-04-12 11:10:18 -0300

    updated package.json

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app=express();
 const viewRouter=require('./routes/viewRoutes');
 const path=require('path');
+const compression=require('compression');
+app.use(compression())
 app.use('/',viewRouter)
 app.use(express.static(path.join(__dirname,'dev-data')))
 app.set('view engine','pug');
