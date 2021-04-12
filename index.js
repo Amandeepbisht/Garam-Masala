eccc1f6b3ebd9abeef76e911c02e1876908cbaa5
Amandeepbisht <amandeepbisht1994@gmail.com>
2021-04-12 14:25:44 -0300

    edited index.js

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ const port=process.env.port||8000;
 app.listen(port,()=>{
   console.log('Listening to requests on port 8000!')
 })
+process.on('unhandeledRejection',err=>{
+  console.log(err.name,err.message)
+  console.log('UNHANDELED REJECTION!...SHUTTING DOWN')
+  server.close(()=>{
+    process.exit(1);
+  })
+})
