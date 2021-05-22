dc84b05a4ce6542155b01a65c6ca1435d70d6e83
Amandeepbisht <amandeepbisht1994@gmail.com>
2021-05-22 19:03:37 -0300

    added return statement

--- dev-data/js/notification_display.js
+++ dev-data/js/notification_display.js
@@ -1,23 +1,25 @@
 let notify=document.querySelector('.notify');
 
 const notification=(str)=>{
-  let arr=window.location.href.split('/')
-  let page=arr[arr.length-1]
-  notify.textContent=str
+  let arr=window.location.href.split('/');
+  let page=arr[arr.length-1];
+  notify.textContent=str;
   if(notify.classList[1]=='error'){
     notify.style.color='red';
   }
   if(page!='signUp'&&page!='login'&&page!='forgotPassword'){
-    setTimeout(function(){
+    console.log('This is the log from line#11 of notification.js')
+    return setTimeout(function(){
       location.reload();
     },3000)
   }
   else{
-    setTimeout(function(){
+    console.log('This is the log from line#17 of notification.js')
+    return setTimeout(function(){
       notify.textContent='';
-      notify.style.color='orange'
+      notify.style.color='orange';
       notify.classList.remove('error');
-    },1500)
+    },3000)
   }
 }
 
