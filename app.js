92bf668d6f8187c04c27b816781a2c97a367e486
Amandeepbisht <amandeepbisht1994@gmail.com>
2021-05-29 19:14:27 -0300

    Updated package.json

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const path=require('path');
 const cors = require('cors');
 const compression=require('compression');
 const cookieParser = require('cookie-parser');
+
+const xss=require('xss-clean');
+const mongoSantize=require('express-mongo-santize')
+const helmet=require('helmet')
+
 app.use(compression())
 app.use(express.json({limit:'10kb'}))
 app.use(express.static(path.join(__dirname,'dev-data')))
 app.use(cookieParser())
 app.use(cors());
 app.options('*', cors());
+
+app.use(xss())
+app.use(mongoSantize());
+app.use(helmet());
+
 app.use(function (req, res, next) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
